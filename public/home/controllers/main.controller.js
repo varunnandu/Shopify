e735498a9af7adb5abacc93938657c15a7b2dc04
Varun Nandu <varunnandu@Rajeshs-MacBook-Pro.local>
2017-09-13 17:52:45 -0400

    First Commit

--- public/home/controllers/main.controller.js
+++ public/home/controllers/main.controller.js
@@ -3,7 +3,7 @@
         .module("Shopify")
         .controller("MainController", MainController);
 
-    function MainController(MainService) {
+    function MainController() {
         var vm = this;
         vm.checkemail = checkemail;
 
@@ -17,24 +17,6 @@
                 vm.error = undefined
             }
         }
-        // vm.registerUser = registerUser;
 
-        // function registerUser(user) {
-        //     UserService
-        //         .findUserByUsername(user.username)
-        //         .success(function () {
-        //             vm.error = "sorry that username is taken"
-        //         })
-        //         .error(function(){
-        //             UserService
-        //                 .createUser(user)
-        //                 .success(function(user){
-        //                     $location.url('/user/' + user._id);
-        //                 })
-        //                 .error(function () {
-        //                     vm.error = 'sorry could not register';
-        //                 });
-        //         });
-        // }
     }
 })();
--- public/services/main.service.js
+++ public/services/main.service.js
@@ -5,7 +5,7 @@
 
     function mainService($http) {
         var api = {
-            "checkemail": checkemail
+            // "checkemail": checkemail
             // "findUserByCredentials": findUserByCredentials,
             // "findUserById": findUserById,
             // "updateUser": updateUser,
@@ -15,10 +15,6 @@
         };
         return api;
 
-        function checkemail(email) {
-            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
-        }
 
     //     function updateUser(userId, newUser) {
     //         return $http.put("/api/user/"+userId, newUser);
